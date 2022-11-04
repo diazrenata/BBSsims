f930746944b4863da0a05c8f4bb857d901a4e467
Renata Diaz <diaz.renatam@gmail.com>
2022-11-04 15:58:23 -0400

    Try coverage

--- data-raw/raw_masses.R
+++
@@ -1,5 +0,0 @@
-## code to prepare `raw_masses` dataset goes here
-
-raw_masses <- read.csv(here::here("dat_to_include", "species_list_with_masses.csv"))
-
-usethis::use_data(raw_masses, overwrite = TRUE)
