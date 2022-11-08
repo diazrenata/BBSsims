6f32575032718dbe9bb5b8bbf813cdcffe94b7ce
Renata Diaz <diaz.renatam@gmail.com>
2022-11-08 10:45:01 -0500

    skme edits and double-checking numeric results

--- analysis/pipelines/pipeline.R
+++ analysis/pipelines/pipeline.R
@@ -58,7 +58,7 @@ all = bind_rows(datasets, methods)
 
 
 ## Set up the cache and config
-db <- DBI::dbConnect(RSQLite::SQLite(), here::here("analysis", "caches", "all_test.sqlite"))
+db <- DBI::dbConnect(RSQLite::SQLite(), here::here("analysis", "caches", "all.sqlite"))
 cache <- storr::storr_dbi("datatable", "keystable", db)
 cache$del(key = "lock", namespace = "session")
 
